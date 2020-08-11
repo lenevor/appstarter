3486307c157ab6e79e2bca7973de6ccee745982a
Alexander Campo <jalexcam@gmail.com>
2020-08-10 19:50:55 -0500

    Changed commentary

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@
 |
 | Here is where the routes for your application are registered. These routes 
 | are loaded from the Router class into a file called "web" from the "routes" 
-| folder of the system's raiza. Do something great!
+| folder of the system's core. Do something great!
 |
 */
 
