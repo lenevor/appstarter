0098febd02d64ea5fc301431fceb4de40cab1be4
Alexander Campo <jalexcam@gmail.com>
2020-08-10 19:23:50 -0500

    Added new file for an api routes

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+|------------------------------------------------------------------------- 
+| Api Routes
+|-------------------------------------------------------------------------
+|
+| Here is where the routes for your application are registered. These routes 
+| are loaded from the Router class into a file called "api" from the "routes" 
+| folder of the system's core. Do something great!
+|
+*/
+
+use Syscodes\Http\Request;
