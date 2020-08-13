ae7b3f57dfb3d76dbea4bc030001f6641b044d61
Alexander Campo <jalexcam@gmail.com>
2020-08-12 21:35:53 -0500

    Added new class

---
+++ app/Http/Controller.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Lenevor Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://lenevor.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@Lenevor.com so we can send you a copy immediately.
+ *
+ * @package     Lenevor
+ * @subpackage  Base
+ * @author      Javier Alexander Campo M. <jalexcam@gmail.com>
+ * @link        https://lenevor.com 
+ * @copyright   Copyright (c) 2019-2020 Lenevor Framework 
+ * @license     https://lenevor.com/license or see /license.md or see https://opensource.org/licenses/BSD-3-Clause New BSD license
+ * @since       0.1.1
+ */
+
+namespace App\Http;
+
+use Syscodes\Controller\Controller as BaseController;
+
+/**
+ * Controller.
+ * 
+ * @author Javier Alexander Campo M. <jalexcam@gmail.com>
+ */
+class Controller extends BaseController
+{
+	//
+}
