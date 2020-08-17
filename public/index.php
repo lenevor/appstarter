10d3f30f328cd2c74e1763af06ea2eb2249192c8
Alexander Campo <jalexcam@gmail.com>
2020-08-17 00:50:07 -0500

    Called of method capture() of Request class

--- public/index.php
+++ public/index.php
@@ -19,7 +19,7 @@
  * @link        https://lenevor.com 
  * @copyright   Copyright (c) 2019-2020 Lenevor Framework 
  * @license     https://lenevor.com/license or see /license.md or see https://opensource.org/licenses/BSD-3-Clause New BSD license
- * @since       0.1.1
+ * @since       0.1.2
  */
 
 /*
@@ -65,7 +65,7 @@ $lenevor = $app->make(Syscodes\Contracts\Core\Lenevor::class);
 
 // Initialize services
 $response = $lenevor->handle(
-    $request = $app->make(Syscodes\Http\Request::class)
+    $request = Syscodes\Http\Request::capture()
 );
 
 // Sends HTTP headers and content
