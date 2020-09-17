7db393714ec4538f1242bcb0ca296036418ebb45
Alexander Campo <jalexcam@gmail.com>
2020-09-16 21:13:38 -0500

    Changed release to v0.7.2

--- public/index.php
+++ public/index.php
@@ -19,7 +19,7 @@
  * @link        https://lenevor.com 
  * @copyright   Copyright (c) 2019-2020 Lenevor Framework 
  * @license     https://lenevor.com/license or see /license.md or see https://opensource.org/licenses/BSD-3-Clause New BSD license
- * @since       0.1.2
+ * @since       0.7.2
  */
 
 /*
