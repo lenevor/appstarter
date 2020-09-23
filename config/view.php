8452e2a4f75d3b2a5183caa98cabf998e841c5f4
Alexander Campo <jalexcam@gmail.com>
2020-09-23 00:22:12 -0500

    Added new key for views path

--- config/view.php
+++ config/view.php
@@ -4,6 +4,21 @@ return [
 
     /*
     |------------------------------------------------------------------------
+    | View Storage Paths
+    |------------------------------------------------------------------------
+    |
+    | Most templating systems load templates from disk. Here you may specif 
+    | an array of paths that should be checked for your views. Of course the 
+    | usual Lenevor view path has already been registered for you. 
+    |
+    */
+
+    'paths' => [
+        resourcePath('views')
+    ],
+
+    /*
+    |------------------------------------------------------------------------
     | Transpiled View Paths
     |------------------------------------------------------------------------
     |
