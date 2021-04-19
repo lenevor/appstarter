adc192fbbfdb464853e38f440532a2f13ae869de
Alexander Campo <jalexcam@gmail.com>
2021-04-19 02:55:01 -0500

    Added content

--- resources/views/welcome.plaze.php
+++ resources/views/welcome.plaze.php
@@ -3,17 +3,29 @@
 <@section('title', __('view.welcomeTo').config('app.name'))
 
 <@section('content')
-	<div class="wrapper">
-		<header>
-			<div class="container">
-				<div class="brand">
-					<h1>{{ config('app.name') }}</h1>
-				</div>
+	<header>
+		<div class="container">
+			<div class="brand">
+				<h1>{{ config('app.name') }}</h1>
 			</div>
-		</header>
-		<div class="content">
-			<div class="container">
-				
+		</div>
+	</header>
+	<div class="content">
+		<div class="container">
+			<div class="status">
+				<h2>{{ __('view.welcomeTo') }} {{ config('app.name') }}</h2>
+				<code>
+					<span class="check">
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+							<path d="M0 0h24v24H0z" fill="none"></path>
+							<path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"></path>
+						</svg>
+					</span>
+					<span>{{ basePath() }}</span>
+				</code>
+				<p class="status-message">
+					{{ __('view.successfullyInstalled') }}
+				</p>
 			</div>
 		</div>
 	</div>
