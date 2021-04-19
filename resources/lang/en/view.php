5abb827cecd6b77910c6ce7f2361324d43a13f6f
Alexander Campo <jalexcam@gmail.com>
2021-04-19 14:25:00 -0500

    Update view.php

--- resources/lang/en/view.php
+++ resources/lang/en/view.php
@@ -26,7 +26,7 @@ return [
     'community-diffusion' => 'Lenevor blog is a community driven portal where you can get news about the framework and including a new package releases and tutorials. Also, you can clone the repository from github.',
     'documentation' => 'Documentation',
     'especification-documentation' => 'Lenevor has a thorough documentation covering all the specifications and features of the framework. Therefore, we        recommended reading all of the documentation referring to the framework.',
-    'successfullyInstalled' => 'Your have successfully installed this application, you can use it now.',
+    'successfullyInstalled' => 'Your have successfully installed this application, you may use it now.',
     'tutorials' => 'Tutorials',
     'video-tutorials' => 'The videos tutorials on Lenevor, check them out, see for yourself and massively level up your development skills in the process.',
     'welcomeTo' => 'Welcome to ',
