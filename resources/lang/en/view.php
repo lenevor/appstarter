73004c6f0f14c88fab11b21eee412f1498496a2c
Alexander Campo <jalexcam@gmail.com>
2021-04-22 00:49:14 -0500

    Removed keys with your respective values

--- resources/lang/en/view.php
+++ resources/lang/en/view.php
@@ -22,13 +22,8 @@ return [
      * View 
      */
 
-    'community' => 'Community',
-    'community-diffusion' => 'Lenevor blog is a community driven portal where you can get news about the framework and including a new package releases and tutorials. Also, you can clone the repository from github.',
-    'documentation' => 'Documentation',
     'especification-documentation' => 'Lenevor has a thorough documentation covering all the specifications and features of the framework. Therefore, we        recommended reading all of the documentation referring to the framework.',
     'successfullyInstalled' => 'Your have successfully installed this application.',
-    'tutorials' => 'Tutorials',
-    'video-tutorials' => 'The videos tutorials on Lenevor, check them out, see for yourself and massively level up your development skills in the process.',
     'welcomeTo' => 'Welcome to ',
 
 ];
