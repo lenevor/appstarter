ada3e4553d84c529ae44d67123c71728e5f08ff3
Alexander Campo <jalexcam@gmail.com>
2022-04-03 23:26:13 -0500

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,5 +6,23 @@ use Syscodes\Components\Database\Erostrine\Model;
 
 class User extends Model
 {
-    // 
+    /**
+     * The attributes that are mass assignable.
+     * 
+     * @var string[] $fillable
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     * 
+     * @var array $hidden
+     */
+    protected $hidden = [
+        'password',
+    ];
 }
