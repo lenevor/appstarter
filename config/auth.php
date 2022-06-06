ecd15d7ac9dd6e9fdae65c765dc107b8bc4774f4
Alexander Campo <jalexcam@gmail.com>
2022-06-05 19:20:05 -0500

    Create auth.php

---
+++ config/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |------------------------------------------------------------------------
+    | Authentication Defaults
+    |------------------------------------------------------------------------
+    |
+    | This option manages the default authentication "guard" and password 
+    | reset options for your application. Also, you can change these default 
+    | values as required, however, it is a perfect start for most applications.
+    |
+    */
+
+    'defaults' => [ 
+        'guard' => 'web'
+    ],
+
+];
