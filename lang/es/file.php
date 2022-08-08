a2d5bb5c8629b0a60140a49fc607a84ccc421c26
Alexander Campo <jalexcam@gmail.com>
2022-08-08 10:50:35 -0500

    Changed path of the lang directory

---
+++ lang/es/file.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |------------------------------------------------------------------------
+    | Archivo de Líneas De Idioma                                                       
+    |------------------------------------------------------------------------
+    |
+    | Las siguientes líneas de idioma contienen los mensajes de error 
+    | predeterminados por la clase Filesystem. Usted es libre de cambiar 
+    | cualquier valor que desee personalizar en estas líneas que mejor se 
+    | adapte a su aplicación.
+    |
+    */
+    
+    'cannotMove' => 'No se pudo mover el archivo {0} hasta {1} ({2})',
+    'identifierEmpty' => 'El archivo identificador del autocargador está vacío!!!',
+    'identifierNotAlpha' => 'Contiene un identificador no alfanumérico [{identifier}]!!!',
+    'identifierLenght' => 'El identificador [{identifier}] debe tener una longitud de 20 dígitos.',
+    'notExistPath' => 'El archivo [{path}] no existe.',
+    'notCreatedDirectory' => 'El directorio [{path}] no se pudo crear.',
+
+];
