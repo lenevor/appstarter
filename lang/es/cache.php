2f299b7919e1295b2ad6f3486bd25ccb0b4d3378
Alexander Campo <jalexcam@gmail.com>
2022-08-08 10:50:12 -0500

    Changed path of the lang directory

---
+++ lang/es/cache.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |------------------------------------------------------------------------
+    | LÍneas De Idioma De Cache                                                     
+    |------------------------------------------------------------------------
+    |
+    | Las siguientes líneas de idioma se usan durante el almacenamiento en 
+    | caché de varios mensajes que tenemos que mostrarle al usuario. Usted es 
+    | libre de modificar estas líneas de idioma de acuerdo con los requisitos 
+    | de su aplicación.
+    |
+    */
+
+    'driverNotSupported' => 'El controlador [{config}] no es compatible.',
+    'storeNotDefined' => 'La memoria caché [{name}] no está definida.',
+
+];
