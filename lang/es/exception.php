a820a39a404d1220b86fa8d086ba1ca634a815ec
Alexander Campo <jalexcam@gmail.com>
2022-08-08 10:50:27 -0500

    Changed path of the lang directory

---
+++ lang/es/exception.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+
+    /*
+    |------------------------------------------------------------------------
+    | Líneas De Idioma De Excepciones                                                     
+    |------------------------------------------------------------------------
+    |
+    | Las siguientes líneas de idioma contienen los mensajes de error 
+    | predeterminados por la vista errorException. Usted es libre de cambiar 
+    | cualquier valor que desee personalizar en estas líneas que mejor se 
+    | adapte a su aplicación.
+    |
+    */
+
+    'arguments' => 'Argumentos',
+    'empty' => 'vacío',
+    'environment' => 'Entorno y detalles:',
+    'environmentVars' => 'Variables de Entorno',
+    'errorHttpCode' => 'No se encontró la vista del código HTTP [{code}].',
+    'files' => 'Archivos',
+    'maxMemoryUsage' => 'Uso máximo de la memoria',
+    'memory' => 'Memoria',
+    'memoryLimit' => 'Limite de memoria',
+    'memoryUsage' => 'Uso de memoria',
+    'noArguments' => 'No hay argumentos para este marco de pila.',
+    'noMessage' => 'Sin mensaje',
+    'openCodeEditor' => 'Abrir el editor de código {editor}',
+    'openReaderPDF' => 'Abrir el lector de PDF',
+    'print' => 'Imprimir',
+    'RegisteredHandlers' => 'Manejadores Registrados',
+    'request' => 'Solicitud',
+    'search' => 'Buscar',
+    'stackFrames' => 'Pila de marcos',
+
+];
