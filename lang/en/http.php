6386718672a034dbd5cc5f3a93efe807ea8cef79
Alexander Campo <jalexcam@gmail.com>
2022-08-08 10:46:38 -0500

    Changed path of the lang directory

---
+++ lang/en/http.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |------------------------------------------------------------------------
+    | Http Language Lines                                                       
+    |------------------------------------------------------------------------
+    |
+    | The following language lines contain the error messages pre-determined 
+    | by the HTTP response. You are free to change any value you want to customize 
+    | in these lines that best match your application.
+    |
+    */
+    
+    'cannotParseURI' => 'Unable to parse URI: {0}',
+    'invalidJson' => 'Failed to parse json string, error: "{0}"',
+    'invalidPort' => 'Ports must be between 0 and 65535. Given: {0}',
+];
