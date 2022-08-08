272ce914aa800d61389f75e6fe8bea5b067a954b
Alexander Campo <jalexcam@gmail.com>
2022-08-08 10:50:19 -0500

    Changed path of the lang directory

---
+++ lang/es/cli.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |------------------------------------------------------------------------
+    | LÍneas De Idioma De CLI                                                   
+    |------------------------------------------------------------------------
+    |
+    | Las siguientes líneas de idioma se usan durante la consola de comandos
+    | de varios mensajes que tenemos que mostrarle al usuario. Usted es 
+    | libre de modificar estas líneas de idioma de acuerdo con los requisitos 
+    | de su aplicación.
+    |
+    */
+
+    'helpArguments' => 'Argumentos:',
+    'helpDescription' => 'Descrición:',
+    'helpOptions' => 'Opciones:',
+    'HelpUsage' => 'Uso:',
+
+];
