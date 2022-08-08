c87adfa2afd8801406b0f59486e15540f88e138c
Alexander Campo <jalexcam@gmail.com>
2022-08-08 10:51:49 -0500

    Changed path of the lang directory

---
+++ lang/es/view.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+
+    /*
+    |------------------------------------------------------------------------
+    | Vista de Líneas De Idioma                                                       
+    |------------------------------------------------------------------------
+    |
+    | Las siguientes líneas de idioma contienen los mensajes de error 
+    | predeterminados por la clase View. Usted es libre de cambiar cualquier 
+    | valor que desee personalizar en estas líneas que mejor se adapte a su 
+    | aplicación.
+    |
+    */
+
+    
+    'dataObjectArray' => 'El parámetro de datos solo acepta objetos y matrices.',
+    'notFound' => 'Vista [{file}] no encontrada.',
+    'rendering' => 'Debe configurar el archivo para usar dentro de su vista antes de renderizar.',
+    'variableNotSet' => 'La variable de vista no está configurada: [{key}].',
+
+    /*
+     * Vista 
+     */
+
+    'contribute-title' => 'Contribuir',
+    'contribute' => 'Lenevor es un proyecto impulsado por mí y accede a contribuciones de código y documentación excelentes para toda la comunidad. <br /> Consulta en <a href="https://github.com/lenevor" target="_blank">github</a> !',
+    'discuss-title' => 'Discutir',
+    'discuss' => 'Lenevor es un proyecto de código abierto de desarrollo comunitario, con varios lugares para que los miembros de la comunidad se reúnan e intercambien ideas. <br />[Esta opción se encuentra en construcción] ',
+    'learn-title' => 'Aprender',
+    'successfullyInstalled' => 'Has instalado correctamente esta aplicación.',
+    'userGuide' => 'La guía del usuario contiene la introducción, el tutorial y la referencia a la documentación de todos los componentes de Lenevor. <br />Consulta la <a href="https://github.com/lenevor/docs" target="\_blank\">documentación</a> !',
+    'welcomeTo' => 'Bienvenido a ',
+
+];
