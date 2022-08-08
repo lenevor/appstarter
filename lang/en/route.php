30ab3ab293d05b485ef2d6398447c96a16275ac0
Alexander Campo <jalexcam@gmail.com>
2022-08-08 10:49:45 -0500

    Changed path of the lang directory

---
+++ lang/en/route.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /*
+    |------------------------------------------------------------------------
+    | Route Language Lines                                                       
+    |------------------------------------------------------------------------
+    |
+    | The following language lines contain the error messages pre-determined 
+    | by the Route class. You are free to change any value you want to customize 
+    | in these lines that best match your application.
+    |
+    */
+    
+    'actionClosureOrFunction' => 'Action should be a Closure or a path to a function',
+    'classNotFound' => 'The [{class}] class not exist',
+    'hasNoAction' => 'Route for [{uri}] has no action',
+    'invalidAction' => 'Invalid route action: [{action}]',
+    'methodNotAllowed' => 'Method not allowed. allowed methods: GET, POST, PUT, DELETE, PATCH, OPTIONS, HEAD, ANY',
+    'methodNotFound' => 'The [{method}] method not exist on the controller',
+    'methodNotProvided' => 'No method provided',
+    'namespaceNotExist' => 'The namespace does not exist or the assignment is null',
+    'paramNoExist' => 'There are no elements in the parameter array',
+    'uriNotProvided' => 'No route provided, for root use /',
+
+];
