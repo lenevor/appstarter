9229b0257a66dc3634c5b609aa047396e916274c
Alexander Campo <jalexcam@gmail.com>
2022-08-08 10:49:23 -0500

    Changed path of the lang directory

---
+++ lang/en/log.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |------------------------------------------------------------------------
+    | Log Language Lines                                                       
+    |------------------------------------------------------------------------
+    |
+    | The following language lines contain the error messages pre-determined 
+    | by the LogManager class. You are free to change any value you want to customize 
+    | in these lines that best match your application.
+    |
+    */
+    
+    'driverNotSupported' => 'Driver [{config}] is not supported.',
+    'invalidLogLevel' => '{0} is an invalid log level.',
+    'LogNotDefined' => 'Log [{name}] is not defined.',
+
+];
