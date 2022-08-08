9031331d53fae15529367a5ad0539c1fd4760169
Alexander Campo <jalexcam@gmail.com>
2022-08-08 10:50:48 -0500

    Changed path of the lang directory

---
+++ lang/es/log.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |------------------------------------------------------------------------
+    | Registro de Líneas De Idioma                                                        
+    |------------------------------------------------------------------------
+    |
+    | Las siguientes líneas de idioma contienen los mensajes de error 
+    | predeterminados por la clase LogManager. Usted es libre de cambiar 
+    | cualquier valor que desee personalizar en estas líneas que mejor se 
+    | adapte a su aplicación.
+    |
+    */
+    
+    'driverNotSupported' => 'El controlador [{config}] no es compatible.',
+    'invalidLogLevel' => '{0} es un nivel de registro no válido.',
+    'LogNotDefined' => 'El registrador [{name}] no está definido.',
+
+];
