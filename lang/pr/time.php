b4e0cbc34abc9420fbc9d827a3e9c2fe4f618e1c
Alexander Campo <jalexcam@gmail.com>
2022-08-25 11:56:18 -0500

    Create time.php

---
+++ lang/pr/time.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+
+     /*
+    |------------------------------------------------------------------------
+    | DateTime Language Lines                                                       
+    |------------------------------------------------------------------------
+    |
+    | The following language lines contain the error messages pre-determined 
+    | by the Chronos class. You are free to change any value you want to customize 
+    | in these lines that best match your application.
+    |
+    */
+
+    'invalidMonth' => 'Os meses devem estar entre 1 e 12. Você colocou erroneamente: {0}',
+    'invalidDay' => 'Os dias devem estar entre 1 e 31. Você colocou erroneamente: {0}',
+    'invalidOverDay' => 'Os dias devem estar entre 1 e {0}. Você colocou erroneamente: {1}',
+    'invalidHour' => 'As horas devem estar entre 0 e 23. Você colocou erroneamente: {0}',
+    'invalidMinutes' => 'Os minutos devem estar entre 0 e 59. Você colocou erroneamente: {0}',
+    'invalidSeconds' => 'Os segundos devem estar entre 0 e 59. Você colocou erroneamente: {0}',
+    'years' => '{0, plural, =1{# year} other{# years}}',
+    'months' => '{0, plural, =1{# month} other{# months}}',
+    'weeks' => '{0, plural, =1{# week} other{# weeks}}',
+    'days' => '{0, plural, =1{# day} other{# days}}',
+    'hours' => '{0, plural, =1{# hour} other{# hours}}',
+    'minutes' => '{0, plural, =1{# minute} other{# minutes}}',
+    'seconds' => '{0, plural, =1{# second} other{# seconds}}',
+    'ago' => '{0} atrás',
+    'inFuture' => 'em {0}',
+    'yesterday' => 'Ontem',
+    'tomorrow' => 'Amanhã',
+    'now' => 'Just now'
+
+];
