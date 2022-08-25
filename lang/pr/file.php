01cce99ee57f5683d86804c0609a48f778dd93e5
Alexander Campo <jalexcam@gmail.com>
2022-08-25 11:56:04 -0500

    Create file.php

---
+++ lang/pr/file.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |------------------------------------------------------------------------
+    | File Language Lines                                                       
+    |------------------------------------------------------------------------
+    |
+    | The following language lines contain the error messages pre-determined 
+    | by the Filesystem class. You are free to change any value you want to 
+    | customize in these lines that best match your application.
+    |
+    */
+    
+    'cannotMove' => 'Não foi possível mover o arquivo {0} para {1} ({2})',
+    'identifierEmpty' => 'O arquivo de identificador do carregador automático está vazio!!!',
+    'identifierNotAlpha' => 'Contém identificador não alfanumérico [{identifier}]!!!',
+    'identifierLenght' => 'O identificador [{identifier}] deve ter 20 dígitos.',
+    'notExistPath' => 'O arquivo [{0}] não existe.',
+    'notCreatedDirectory' => 'O diretório [{0}] não pôde ser criado.', 
+
+];
