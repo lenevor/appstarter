1b6637fe259b6bd7e4b9e4a03942f854e64344d4
Alexander Campo <jalexcam@gmail.com>
2022-08-25 11:56:13 -0500

    Create response.php

---
+++ lang/pr/response.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |------------------------------------------------------------------------
+    | Response Language Lines                                                       
+    |------------------------------------------------------------------------
+    |
+    | The following language lines contain the error messages pre-determined 
+    | by the Response class. You are free to change any value you want to customize 
+    | in these lines that best match your application.
+    |
+    */
+    
+    'missingStatusCode' => 'A resposta HTTP está faltando um código de status.',
+    'notFoundLevel' => 'Não foi encontrado [{level}] como um nível de log.',
+    'statusCodeNotValid' => 'O código de estado HTTP [{code}] não é válido.',
+    'UnknownStatus' => 'Status desconhecido',
+
+];
