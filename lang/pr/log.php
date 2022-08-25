b308cb5710d41593bc3875611d8e2d884a2eebbe
Alexander Campo <jalexcam@gmail.com>
2022-08-25 11:56:10 -0500

    Create log.php

---
+++ lang/pr/log.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |------------------------------------------------------------------------
+    | Log Language Lines                                                       
+    |------------------------------------------------------------------------
+    |
+    | The following language lines contain the error messages pre-determined 
+    | by the LogManager class. You are free to change any value you want to customize 
+    | in these lines that best match your application.
+    |
+    */
+    
+    'driverNotSupported' => 'O driver [{config}] não é compatível.',
+    'invalidLogLevel' => '{0} é um nível de registro inválido.',
+    'LogNotDefined' => 'Log [{name}] não está definido.',
+
+];
