ddcfddd20320813fb08673b9fc4926e5aa802ff9
Alexander Campo <jalexcam@gmail.com>
2022-08-25 11:56:08 -0500

    Create http.php

---
+++ lang/pr/http.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |------------------------------------------------------------------------
+    | Http Language Lines                                                       
+    |------------------------------------------------------------------------
+    |
+    | The following language lines contain the error messages pre-determined 
+    | by the HTTP response. You are free to change any value you want to customize 
+    | in these lines that best match your application.
+    |
+    */
+    
+    'cannotParseURI' => 'Não é possível analisar URI: {0}',
+    'invalidJson' => 'Falha ao analisar a sequência json, erro: "{0}"',
+    'invalidPort' => 'As portas devem estar entre 0 e 65535. Dado: {0}',
+];
