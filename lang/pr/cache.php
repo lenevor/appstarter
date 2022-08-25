aa6fca5da69d8eea183708b570caa2305b878538
Alexander Campo <jalexcam@gmail.com>
2022-08-25 11:55:57 -0500

    Create cache.php

---
+++ lang/pr/cache.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |------------------------------------------------------------------------
+    | Cache Language Lines                                                       
+    |------------------------------------------------------------------------
+    |
+    | The following language lines are used during caching for several messages 
+    | that we need to show the user. You are free to modify these language lines 
+    | according to your application's requirements.
+    |
+    */
+
+    'driverNotSupported' => 'O driver [{config}] não é compatível',
+    'storeNotDefined' => 'O cache [{name}] não foi definido',
+
+];
