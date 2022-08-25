0ea03afc8953c9f592430be3c31888d5ca856505
Alexander Campo <jalexcam@gmail.com>
2022-08-25 11:56:02 -0500

    Create exception.php

---
+++ lang/pr/exception.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+
+    /*
+    |------------------------------------------------------------------------
+    | Exception Language Lines                                                       
+    |------------------------------------------------------------------------
+    |
+    | The following language lines contain the error messages pre-determined 
+    | by the errorException view. You are free to change any value you want to 
+    | customize in these lines that best match your application.
+    |
+    */
+
+    'arguments' => 'Argumentos',
+    'empty' => 'vazio',
+    'environment' => 'Ambiente e detalhes:',
+    'environmentVars' => 'Variáveis de ambiente',
+    'errorHttpCode' => 'Não foi encontrada a visualização do Código HTTP [{code}]',
+    'files' => 'Arquivos',
+    'maxMemoryUsage' => 'Pico de uso de memória',
+    'memory' => 'Memória',
+    'memoryLimit' => 'Limite da memória',
+    'memoryUsage' => 'Uso da Memória',
+    'noArguments' => 'Sem argumentos para este quadro de pilha',
+    'noMessage' => 'Nenhuma mensagem',
+    'openCodeEditor' => 'Abra o editor de código {editor}',
+    'openPDFReader' => 'Abra o leitor de PDF',
+    'print' => 'Impressão',
+    'RegisteredHandlers' => 'Manipuladores cadastrados',
+    'request' => 'Pedido',
+    'search' => 'Buscar',
+    'stackFrames' => 'Empilhar quadros',
+
+];
