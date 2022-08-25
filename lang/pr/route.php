4f32e668fc469603be343524e8b208add81cbd65
Alexander Campo <jalexcam@gmail.com>
2022-08-25 11:56:16 -0500

    Create route.php

---
+++ lang/pr/route.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /*
+    |------------------------------------------------------------------------
+    | Route Language Lines                                                       
+    |------------------------------------------------------------------------
+    |
+    | The following language lines contain the error messages pre-determined 
+    | by the Route class. You are free to change any value you want to customize 
+    | in these lines that best match your application.
+    |
+    */
+    
+    'actionClosureOrFunction' => 'A ação deve ser um Closure ou um caminho para uma função',
+    'classNotFound' => 'A classe [{class}] não existe',
+    'hasNoAction' => 'Rota para [{uri}] não tem ação',
+    'invalidAction' => 'Ação de rota inválida: [{action}]',
+    'methodNotAllowed' => 'Método não permitido. Métodos permitidos: GET, POST, PUT, DELETE, PATCH, OPTIONS, HEAD, ANY',
+    'methodNotFound' => 'O método [{method}] não existe no controlador',
+    'methodNotProvided' => 'Nenhum método fornecido',
+    'namespaceNotExist' => 'O espaço de nomes não existe ou a atribuição é nula',
+    'paramNoExist' => 'Não há elementos na matriz de parâmetros',
+    'uriNotProvided' => 'Nenhuma rota fornecida, para uso raiz /',
+
+];
