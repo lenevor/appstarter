fdbab2a4438012b4ba5d277f557bbec2a02efcf0
Alexander Campo <jalexcam@gmail.com>
2022-08-25 11:56:22 -0500

    Create view.php

---
+++ lang/pr/view.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    /*
+    |------------------------------------------------------------------------
+    | View Language Lines                                                       
+    |------------------------------------------------------------------------
+    |
+    | The following language lines contain the error messages pre-determined 
+    | by the View class. You are free to change any value you want to customize 
+    | in these lines that best match your application.
+    |
+    */
+   
+    'dataObjectArray' => 'O parâmetro data só aceita objetos e arrays.',    
+    'notFound' => 'Ver [{file}] não encontrado.',    
+    'rendering' => 'Você deve definir o arquivo a ser usado em sua visualização antes de renderizar.',
+    'variableNotSet' => 'A variável de visualização não está definida: [{key}].',   
+
+    /*
+     * View 
+     */
+
+    'contribute-title' => 'Contribuir',
+    'contribute' => 'Lenevor é um projeto orientado a mim e acessa contribuições de código e documentação ótimas para toda a comunidade. <br />Verifica a <a href="https://github.com/lenevor" target="_blank">github</a> !',
+    'discuss-title' => 'Discutir',
+    'discuss' => 'Lenevor é um projeto de código aberto de desenvolvimento comunitário, com vários locais para os membros da comunidade se reunirem e trocarem ideias. <br />[ Esta opção pode ser encontrada em construção ]',
+    'learn-title' => 'Aprender',
+    'successfullyInstalled' => 'Você instalou este aplicativo com sucesso.',
+    'userGuide' => 'O guia do usuário contém a introdução, tutorial e referência à documentação de todos os componentes Lenevor. <br />Verifica a <a href="https://github.com/lenevor/docs" target="\_blank\">documentação</a> !',
+    'welcomeTo' => 'Bem-vindo ao ',
+
+];
