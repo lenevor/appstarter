32d6a723096ceb434d55a0d9b0ef22c922dae851
Alexander Campo <jalexcam@gmail.com>
2022-10-29 08:20:52 -0500

    Update Authenticate.php

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -40,7 +40,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if ( ! $request->expectsJson()) {
-            return route('login');
+            return redirect()->route('login');
         }
     }
 }
