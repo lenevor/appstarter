b2617bf9372525adffb13103341ef3acd702e60e
Alexander Campo <jalexcam@gmail.com>
2024-04-04 11:57:44 -0500

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,54 +1,18 @@
 <?php 
 
-/*
-|------------------------------------------------------------------------
-| Create the application
-|------------------------------------------------------------------------
-|
-| What we will do is create a new instance of a Lenevor application that 
-| serves to request all the Lenevor components and is the IoC container 
-| for the system that joins all the parts of the framework.
-|
-*/
+use Syscodes\Components\Core\Application;
 
-$app = new Syscodes\Components\Core\Application(
-    $_ENV['APP_ROOT_PATH'] ?? dirname(__DIR__)
-);
-
-/*
-|------------------------------------------------------------------------
-| Bind Important Interfaces
-|------------------------------------------------------------------------
-|
-| Next, we need to bind some important interfaces into the container for 
-| be will be able to resolve the incomming requests to this application 
-| from both the web and CLI.
-|
-*/
-
-$app->singleton(
-    Syscodes\Components\Contracts\Http\Lenevor::class, 
-    App\Http\Lenevor::class
-);
-
-$app->singleton(
-    Syscodes\Components\Contracts\Console\Lenevor::class, 
-    App\Console\Lenevor::class
-);
-
-$app->singleton(
-    Syscodes\Components\Contracts\Debug\ExceptionHandler::class, 
-    App\Exceptions\Handler::class
-);
-
-/*
-|------------------------------------------------------------------------
-| Return The Application
-|------------------------------------------------------------------------
-|
-| This variable returns the application instance for the building of 
-| instances from the running of the application and sending responses.
-|
-*/
-
-return $app;
+return Application::configure(basePath: dirname(__DIR__))
+    /*
+    |------------------------------------------------------------------------
+    | Bind Important Interfaces
+    |------------------------------------------------------------------------
+    |
+    | Next, we need to bind some important interfaces into the container for 
+    | be will be able to resolve the incomming requests to this application 
+    | from both the web and CLI.
+    |
+    */
+    ->assignExceptions(function () {
+        //
+    })->create(); // Return the application
