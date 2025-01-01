0d21877a3b19c4ef49c802877a1c1e60e2ee41c8
Alexander Campo <jalexcam@gmail.com>
2025-01-01 03:06:58 -0500

    Updated release of copyright 2024 to 2025

--- app/Http/Controller.php
+++ app/Http/Controller.php
@@ -15,7 +15,7 @@
  *
  * @package     Lenevor
  * @link        https://lenevor.com
- * @copyright   Copyright (c) 2019 - 2024 Alexander Campo <jalexcam@gmail.com>
+ * @copyright   Copyright (c) 2019 - 2025 Alexander Campo <jalexcam@gmail.com>
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
