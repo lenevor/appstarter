9aaded41ad78aa272e080a4ce6034260b1882227
Alexander Campo <jalexcam@gmail.com>
2025-11-21 15:30:57 -0500

    Changed namespace

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,19 @@
 <?php
 
-use Syscodes\Components\Core\Inspiring;
+use Syscodes\Components\Core\Inspiration;
 use Syscodes\Components\Support\Facades\Prime;
 
+/*
+|------------------------------------------------------------------------- 
+| Console Routes
+|-------------------------------------------------------------------------
+|
+| This file is where you may define all of your Closure based console 
+| commands. Each Closure is bound to a command instance allowing a simple 
+| approach to interacting with each command's IO methods.
+|
+*/
+
 Prime::command('inspire', function () {
-    $this->comment(Inspiring::quote());
+    $this->comment(Inspiration::quote());
 })->describe('Display an inspiring quote');
