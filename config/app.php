a43cdd2847608014f23a114bf5e453daf2151213
Alexander Campo <jalexcam@gmail.com>
2026-06-01 01:00:22 -0500

    Formatting

--- config/app.php
+++ config/app.php
@@ -125,25 +125,18 @@ return [
     |-----------------------------------------------------------------------
     |
     | The application locale determines the default locale that will be used
-    | by the translation service provider. You are free to set this value to 
-    | any of the locales which will be supported by the application.
+    | by Lenevor's translation / localization methods. You are free to set 
+    | this value to any of the locales which will be supported by the 
+    | application.
     |
     */
 
-    'locale' => 'en',
+    'locale' => env('APP_LOCALE', 'en'),
 
-    /*
-    |-----------------------------------------------------------------------
-    | Application Fallback Locale                                                    
-    |-----------------------------------------------------------------------
-    |
-    | The fallback locale determines the locale to use when the current one
-    | is not available. You may change the value to correspond to any of the 
-    | language folders that are provided through your application.
-    |
-    */
+    'fallbackLocale' => env('APP_FALLBACK_LOCALE', 'en'),
+    
+    'fibber_locale' => env('APP_FIBBER_LOCALE', 'en_US'),
 
-    'fallbackLocale' => 'en',
 
     /*
 	|-----------------------------------------------------------------------
